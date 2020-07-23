853abf7ab6e680cc5caea67b8bf46a00b7516523
Santiago Castillo <sancastillog@gmail.com>
2020-07-23 14:47:34 +0000

    returning null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,6 +17,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 	Lnode = binary_tree_node(parent, value);
+	if (Lnode == NULL)
+		return (NULL);
 	parent->left = Lnode;
 	return (Lnode);
 }
