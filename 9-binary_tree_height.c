f152633f9f9f30a823696dd771ac81fa0cc31a94
Santiago Castillo <sancastillog@gmail.com>
2020-07-25 01:10:09 +0000

    betty for 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,13 +13,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left >= right)
+		return (left + 1);
 	else
-	{
-		left = binary_tree_height(tree->left);
-		right = binary_tree_height(tree->right);
-		if (left >= right)
-			return (left + 1);
-		else
-			return (right + 1);
-	}
+		return (right + 1);
 }
