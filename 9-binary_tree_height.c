41e4798bcf2e3cd8ca55a50c225d3c2ac985e4a3
Santiago Castillo <sancastillog@gmail.com>
2020-07-25 00:08:44 +0000

    task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measure heihgt of a binary tree
+ * @tree: pointer to the root node
+ *
+ * Return: if tree is NULL return 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	else
+	{
+		left = binary_tree_height(tree->left);
+		right = binary_tree_height(tree->right);
+		if (left >= right)
+			return (left + 1);
+		else
+			return (right + 1);
+	}
+}
